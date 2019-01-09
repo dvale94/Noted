a803f94043efd70914329fcfd5b1e300e6b095a6
Daniel Valencia <valenciad1994@gmail.com>
2019-01-09 13:03:18 -0500

    added the ability to create and read a note

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+
 const app            = express();
 
 //=========================//
@@ -11,7 +13,14 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) {
+    return console.log(err)
+  }
+  
+  require('./app/routes')(app, database);
+  
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
