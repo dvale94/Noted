7b3cbca6e511f9c681c1bb1c318250e236dc5cf0
Daniel Valencia <valenciad1994@gmail.com>
2019-01-09 13:29:34 -0500

    added the ability to update and delete a note from the databse

--- app/routes/noted_routes.js
+++ app/routes/noted_routes.js
@@ -1,6 +1,7 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
+  //====READ note from database====//
   app.get('/notes/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id)};
@@ -14,6 +15,7 @@ module.exports = function(app, db) {
     });
   });
 
+  //====CREATE note from database====//
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
       db.collection('notes').insert(note, (err, result) => {
@@ -24,5 +26,32 @@ module.exports = function(app, db) {
           res.send(result.ops[0]);
         }
       });
+  });
+
+  //====DELETE note from database====//
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
+
+  //====UPDATE note from database====//
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
   });
 };
